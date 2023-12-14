58e19ee15d2c67133545e9e6612431c0ecbfcde8
Leonardo Martins <leonardogustavo@outlook.com>
2023-12-14 15:39:59 -0300

    Fixed the GetGamesPlayedBY feature

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -24,9 +24,8 @@ public class TrybeGamesDatabase
     // 5. Crie a funcionalidade de buscar jogos jogados por uma pessoa jogadora
     public List<Game> GetGamesPlayedBy(Player player)
     {
-        var gamesPlayed = from id in player.GamesOwned
-                          join game in Games
-                          on id equals game.Id
+        var gamesPlayed = from game in Games
+                          where game.Players.Contains(player.Id)
                           select game;
 
         return gamesPlayed.ToList();
@@ -36,9 +35,9 @@ public class TrybeGamesDatabase
     public List<Game> GetGamesOwnedBy(Player playerEntry)
     {
         var gamesOwned = from id in playerEntry.GamesOwned
-                          join game in Games
-                          on id equals game.Id
-                          select game;
+                         join game in Games
+                         on id equals game.Id
+                         select game;
 
         return gamesOwned.ToList();
     }
@@ -47,8 +46,12 @@ public class TrybeGamesDatabase
     // 7. Crie a funcionalidade de buscar todos os jogos junto do nome do estúdio desenvolvedor
     public List<GameWithStudio> GetGamesWithStudio()
     {
-        // Implementar
+        // var result = from game in Games
+        //              join studio in GameStudios
+        //              on game.DeveloperStudio equals studio.Id
+        //              join
         throw new NotImplementedException();
+
     }
 
     // 8. Crie a funcionalidade de buscar todos os diferentes Tipos de jogos dentre os jogos cadastrados
